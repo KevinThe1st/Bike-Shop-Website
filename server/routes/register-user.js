aa3fb6369f9388c730336cd7887ddea300bea2f7
Sean Mendonca <smendonc@gmail.com>
2018-10-21 15:51:48 -0700

    user routes wip

---
+++ server/routes/register-user.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const { User } = require('../models');
+
+/* CREATE user. */
+router.put('/', function(req, res) {
+  const {
+    username,
+    password,
+    lastName,
+    firstName,
+    type,
+  } = req.body;
+  User.create({
+    username,
+    password,
+    lastName,
+    firstName,
+    type,
+  }).then((item) => {
+    res.json({ created: 'Success' });
+  }).catch(() => {
+    //// TODO: check if user already made and return appropiate message
+    res.json({ created: 'Failure' });
+  });
+});
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const { User } = require('../models');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  User.findAll().then((items) => {
+    res.json({items});
+  });
+});
+
+/* GET specific user listing. */
+router.get('/:id', function(req, res) {
+  User.findById(req.params.id).then((item) => {
+    res.json({item});
+  });
+});
+
+router.delete('/:id', function(req, res) => {
+  const idToDelete = req.params.id;
+  //// TODO: make sure person calling is admin
+  Item.findById(idToDelete).then((item) => {
+    item.destroy().then(() => {
+      res.json({ delete: true });
+    });
+  }).catch(() => {
+    res.json({ delete: false });
+  });
 });
 
 module.exports = router;
