2e354243c2e96acbc8c2ead9597b341f50fce991
KevinThe1st <klabel@calpoly.edu>
2018-10-21 15:13:55 -0700

    Added frontend changes

---
+++ app/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <h1>Welcome to Foxcycle Cyclery</h1>
+    <h3>Thank you for shopping with us</h3>
+    <h3>Please login or register to continue</h3>
+    <input type = "text"  id = "loginUsernameInput" placeholder = "Username">
+    <input type = "submit" id = "loginUsernameButton">
+    <br>
+    <input type = "text"  id = "loginPasswordInput" placeholder = "Password">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {}
+</script>
