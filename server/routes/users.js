aa50a1889392dc20c85da1a88f78af99c283d74a
KevinThe1st <klabel@calpoly.edu>
2018-10-23 15:59:09 -0700

    Added changes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var { User } = require('../models');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post((req, res) => {
+  const {
+    username,
+    password,
+    lastName,
+    firstName,
+    type,
+    createdAt,
+    updatedAt,
+  } = req.body;
+  // validate potentially here
+  User.create({
+    username,
+    password,
+    lastName,
+    firstName,
+    type,
+    createdAt,
+    updatedAt,
+  }).then((user) => {
+    res.json(user);
+  });
 });
 
 module.exports = router;
