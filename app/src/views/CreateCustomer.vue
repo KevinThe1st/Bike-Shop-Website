38bcf0d3f6035579dc1c6673ba5c7e1cd5ca4bd8
Sean Mendonca <smendonc@gmail.com>
2018-10-23 18:00:11 -0700

    front end fixes

--- app/src/views/CreateCustomer.vue
+++ app/src/views/CreateCustomer.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="createcustomer">
+  <div class="CreateCustomer">
     <h1>Create a New Customer Account</h1>
     <h3>Please enter a username and password:</h3>
-    <input type = "text"  id = "createUserUsernameInput" placeholder = "Username" v-model="UsernameMessage">
-    <input type = "submit" id = "createUserButton" v-on:click="updateDone"/>
+    <input class="input" type="text" placeholder="Username" v-model="UsernameMessage">
+    <input type="submit" id="createUserButton" v-on:click="updateDone"/>
     <br>
-    <input type = "text"  id = "createUserPasswordInput" placeholder = "Password" v-model="PasswordMessage">
-    <input type = "text"  id = "createUserFirstNameInput" placeholder = "First Name" v-model="FirstNameMessage">
-    <input type = "text"  id = "createUserLastNameInput" placeholder = "Last Name" v-model="LastNameMessage">
+    <input class="input" type="text" placeholder="Password" v-model="PasswordMessage">
+    <input class="input" type="text" placeholder="First Name" v-model="FirstNameMessage">
+    <input class="input" type="text" placeholder="Last Name" v-model="LastNameMessage">
   </div>
 </template>
 
@@ -18,16 +18,20 @@ import { CreateUserItem } from '@/models';
 
 @Component
 export default class CreateCustomer extends Vue {
-  user!: CreateUserItem;
-  updateDone (event: any) {
-
-    //console.log({{ UsernameMessage }});
-    //if (this.user) {
-      //let checked = event.target.checked;
-      //axios.put(`/api/users/${this.user.id}`, {...this.user, username: {{ UsernameMessage }}, password: {{ PasswordMessage }}, type: "customer", lastName: {{ LastNameMessage }}, firstName: {{ FirstNameMessage }}}).then((res) => {
-        //this.todo = res.data; //this is not accurate
-      //})
-    //}
+  user: CreateUserItem|null = null;
+  UsernameMessage: string = '';
+  PasswordMessage: string = '';
+  FirstNameMessage: string = '';
+  LastNameMessage: string = '';
+  updateDone () {
+    axios.put(`/api/users`, {
+      username: this.UsernameMessage,
+      password: this.PasswordMessage,
+      lastName: this.LastNameMessage,
+      firstName: this.FirstNameMessage
+    }).then((res) => {
+      this.user = res.data; //this is not accurate
+    })
   }
 }
 </script>
