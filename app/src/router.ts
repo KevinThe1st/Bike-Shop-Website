a7d3140a541becc4f0e5eaacfd28a8869b8dcc3e
Kyle Chin <kylechin01@gmail.com>
2018-10-23 17:22:04 -0700

    Popup for logins

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import FoxcycleHome from './views/FoxcycleHome.vue';
 
 Vue.use(Router);
 
@@ -12,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: FoxcycleHome,
+      component: Home,
     },
     {
       path: '/about',
@@ -23,10 +22,12 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
     {
-      path: '/login',
-      name: 'login',
-      //redirect to login page
-      component: () => import(/* webpackChunkName: "login" */ './views/Login.vue'),
+      path: '/register',
+      name: 'register',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Register.vue'),
     },
   ],
 });
