e38b9eb0319d11e4ee407d627c51a37bec4584f1
KevinThe1st <klabel@calpoly.edu>
2018-11-01 18:17:40 -0700

    Added slideshow Kevin

---
+++ app/src/components/Slider.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <vue-flux
+   :options="fluxOptions"
+   :images="fluxImages"
+   :transitions="fluxTransitions"
+   ref="slider">
+    </vue-flux>
+
+    <button @click="$refs.slider.showImage('next')">NEXT</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { VueFlux, FluxPagination, Transitions } from 'vue-flux';
+
+export default {
+   components: {
+      VueFlux,
+      FluxPagination
+   },
+
+   data: () => ({
+      fluxOptions: {
+         autoplay: true
+      },
+      fluxImages: [
+         '../assets/FoxcycleLogoKevin.png',
+         '../assets/FoxcycleLogoKevin.png',
+         '../assets/FoxcycleLogoKevin.png'
+      ],
+      fluxTransitions: {
+         transitionBook: Transitions.transitionBook
+      }
+   })
+}
