d794d7f1304effcab01b6827d053ff80708e4e31
Kyle Chin <kylechin01@gmail.com>
2018-11-01 18:19:58 -0700

    WIP

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,110 +1,30 @@
 <template>
   <div id="app">
     <div id="nav">
-      <!--
-        <img src="./assets/logo.png" alt="logo" id="logo">
-      -->
-      <router-link to="/">Home</router-link>
-      <router-link to="/shop">Shop</router-link>
-      <router-link to="/services">Services</router-link>
-      <router-link to="/about">About</router-link>
-      <div id="login">
-        <router-link to="" v-on:click.native="showModal" is-active=false>Login</router-link>
-      </div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link>
     </div>
-    <LoginPopup
-      v-show="isPopupVisible"
-      @close="closeModal"
-    />
     <router-view/>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
-import LoginPopup from '@/components/LoginPopup.vue';
-
-@Component({
-  components: {
-    LoginPopup
-  },
-  props:{
-    loggedIn: Boolean = false
-  }
-})
-export default class App extends Vue {
-  isPopupVisible: boolean = false;
-
-  showModal() {
-    this.isPopupVisible = true;
-    console.log(this.loggedIn);
-  }
-  closeModal() {
-    this.isPopupVisible = false;
-  }
-}
-</script>
 
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
-
-#logo {
-  padding: 0px 20px;
-  width: 20px;
-  height: 20px;
-}
-
 #nav {
-  list-style-type: none;
-  background-color: #f1f1f1;
-  display: inline;
-  background-color: #333;
-  top: 0;
-  left: 0;
-  width: 100%;
-  position: fixed;
-
+  padding: 30px;
   a {
-    display: inline-block;
-    padding: 8px 16px;
     font-weight: bold;
-    text-decoration: none;
-    text-align: center;
-    padding: 14px 16px;
-    color: white;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
-
-  a:hover {
-    background-color: #555;
-    color: white;
-  }
-
-  a.router-link-exact-active {
-    background-color: #4CAF50;
-    color: white;
-  }
-}
-
-#login {
-  position: absolute;
-  left: 90%;
-  top: 0px;
-  background-color: #f1f1f1;
-
-  a {
-    background-color: #002288;
-  }
-
-  a.router-link-exact-active {
-    background-color: #002288;
-    color: white;
-  }
-
 }
 </style>
