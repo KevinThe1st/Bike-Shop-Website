ffece30be34f9ec4553a00040e83077d7427a335
Sean Mendonca <smendonc@gmail.com>
2018-11-12 12:51:20 -0800

    seeds wip

--- server/seeders/20181109044822-testData.js
+++ server/seeders/20181109044822-testData.js
@@ -1,26 +1,52 @@
 'use strict';
 
+// const { User, Session, Address, Order } = require('../models');
+//
+// const models = [Address, Session, User, Order];
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
+    return Promise.all([
+      queryInterface.bulkInsert('Users', [{
+        username: 'admin',
+        password: 'password',
+        lastName: 'Fox',
+        firstName: 'John',
+        type: 'Admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {}),
+      queryInterface.bulkInsert('Users', [{
+        username: 'emp',
+        password: 'password',
+        lastName: 'Rotten',
+        firstName: 'Robbie',
+        type: 'Employee',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {}),
+      queryInterface.bulkInsert('Users', [{
+        username: 'test@test.com',
+        password: 'password',
+        lastName: 'Jobs',
+        firstName: 'Steve',
+        type: 'Customer',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {}),
+  ]);
+},
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return Promise.all([
+      // models.map(model =>
+      //   model.destroy({
+      //     where: {},
+      //     force: true
+      //   })
+      // )
+      queryInterface.bulkDelete('Sessions', null, {}),
+      queryInterface.bulkDelete('Users', null, {})
+    ]);
   }
 };
