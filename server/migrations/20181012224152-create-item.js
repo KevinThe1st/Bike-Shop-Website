aa0d2cc25508e508dddf0c07936d1159e01bfa88
Sean Mendonca <smendonc@gmail.com>
2018-11-15 19:37:50 -0800

    changed to allow long descriptions

--- server/migrations/20181012224152-create-item.js
+++ server/migrations/20181012224152-create-item.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       descLong: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
