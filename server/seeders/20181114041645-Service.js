1086c71763ef715dab5415ab9fc5fd0267953488
Sean Mendonca <smendonc@gmail.com>
2018-11-15 18:32:45 -0800

    Merge remote-tracking branch 'origin/justin' into SeanMendonca

--- server/seeders/20181114041645-Service.js
+++ server/seeders/20181114041645-Service.js
@@ -3,14 +3,23 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-    queryInterface.bulkInsert('Services', [{
-      name: 'Get your bike serviced',
-      desc: 'Get your trusty little companion thoroughly serviced. Prices range from $5 - $100',
-      picName: '../assets/homepagenew6.png',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {}),
-  ]);
+      queryInterface.bulkInsert('Services', [
+        {
+          name: 'Get your bike serviced',
+          desc: 'Get your trusty little companion thoroughly serviced. Prices range from $5 - $100',
+          picName: '../assets/homepagenew6.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Free tune-up',
+          desc: 'Every bicycle sold at Foxcycle includes a free tune within 6 months of purchase.',
+          picName: '../assets/homepagenew6.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ], {}),
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
