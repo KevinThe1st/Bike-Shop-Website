ecee84ef8c5d480cbb2feb0a49b8bcb43ac8139c
KevinThe1st <klabel@calpoly.edu>
2018-11-29 15:50:57 -0800

    added style and functionality to cart page

--- app/src/views/Cart.vue
+++ app/src/views/Cart.vue
@@ -1,10 +1,106 @@
 <template>
-  <div id="cart">
+  <div id = "cart">
+    <div id="topCartContainer">
+        <div>
+          <div class = "col-sm-12">
+            <h1 id="CartTopText">Cart Overview</h1>
+          </div>
+        </div>
+    </div>
+
+    <div class = "container">
+
+      <div class = "row">
+
+        <div class = "col-sm-1"></div>
+
+        <div class = "col-sm-8">
+
+            <div class = "container">
+
+              <div class = "row">
+                <div id="FoxcycleLogoCart">
+                    Foxcycle
+                </div>
+              </div>
+              <div class = "row">
+                <div>
+                    <!-- display number of items here-->
+                </div>
+              </div>
+
+              <br>
+
+
+              <div class = "row">
+                <div class = "col-sm-4">
+                    Item
+                </div>
+                <div class = "col-sm-2"></div>
+                <div class = "col-sm-2">
+                    price
+                </div>
+                <div class = "col-sm-2"></div>
+                <div class = "col-sm-2">
+                    amount
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class = "col-sm-3"></div>
+
+      </div>
+      <div class = "row">
+        <ul class ="noCartBullets">
+          <li id="item-names" v-for="(item, index) in orderItemsInCurrentOrderWithItemData">
+            <div class = "cartItem">
+                <div class = "container">
+                    <div class = "row cartItemName">
+
+                        <div class = "col-sm-7">
+                          {{item.name}}
+                        </div>
+                        <div class = "col-sm-5"></div>
+                    </div>
+                    <div class = "row">
+                        <div class = "col-sm-12 itemIndex">
+                          item id: {{index}}
+                        </div>
+                    </div>
+                    <div class = "row">
+
+                        <div class = "col-sm-4">
+                            <img v-bind:src="item.picName" width=100%>
+                        </div>
+                        <div class = "col-sm-2"></div>
+                        <div class = "col-sm-2 priceStyle">
+                            ${{item.price}}
+                        </div>
+                        <div class = "col-sm-2"></div>
+                        <div class = "col-sm-2 quantityStyle">
+                            {{item.quantity}}
+                        </div>
+                    </div>
+
+              </div>
+            </div>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
+
+
+
+
+
     <input type="button" v-on:click="getUserOrder()">
     <input type="button" v-on:click="getCombinedData()">
-    <li id="item-names" v-for="(item, index) in orderItemsInCurrentOrderWithItemData">
-      item {{index}}: name = {{item.name}}, quantity = {{item.quantity}}, price = {{item.price}}, picName = {{item.picName}}
-    </li>
+
+
+
   </div>
 </template>
 
@@ -78,6 +174,70 @@ export default class Cart extends App {
 <style lang="scss">
 #cart {
   padding: 80px 0px;
-  text-align: center;
 }
+
+#topCartContainer {
+  border-bottom: 2px #EDEDED solid;
+}
+
+#CartTopText {
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.noCartBullets {
+  ul {
+    list-style-type: none;
+  }
+  li {
+    list-style-type: none;
+  }
+}
+
+.cartItem {
+  background-color: white;
+  border: 1px black solid;
+  padding: 5px;
+  margin: 4px;
+  width: 100%;
+}
+
+.cartItem:hover {
+  background-color: #DCDCDC;
+}
+
+.cartItemName {
+  font-weight: bold;
+  font-size: 25px;
+  color: blue;
+}
+
+.itemIndex {
+  font-style: italic;
+  color: grey;
+}
+
+.priceStyle {
+  color: red;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.quantityStyle {
+  color: black;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+#FoxcycleLogoCart {
+  color: #FFD700;
+  font-family: "Cambria", "Courier New", "Times New Roman", serif;
+  font-style: italic;
+  font-size: 30px;
+}
+
+
+
+
 </style>
