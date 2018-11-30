69b0c0f4d19629e262aae844597828a3622a6421
Kyle Chin <kylechin01@gmail.com>
2018-11-29 16:16:51 -0800

    new and improved categories

--- app/src/views/Shop.vue
+++ app/src/views/Shop.vue
@@ -8,32 +8,31 @@
         </div>
     </div>
 
-
     <div>
       <div class = "row">
           <div class = "col-sm-2">
                 <div id="category-bar">
                   <p id="CategoriesTopText">Categories</p>
-                  <div id="applied-categories">
+                  <div id="top-level-categories" v-if="loadedTopLevelCategoryCount == topLevelCategories.length">
                     <ul class ="noBullets">
-                      <li v-for="(category, index) in appliedCategories">
-                        <input type="checkbox" v-on:click="getSuperCategories(index)" v-bind:checked="true">{{category.name}}
+                      <li v-for="(category, categoryIndex) in topLevelCategories">
+                        {{category.name}}
+                        <ul class ="noBullets">
+                          <li v-for="(subcategory, subcategoryIndex) in category.subcategories">
+                            <input type="checkbox" v-on:click="getItemsRefinedByCategories(categoryIndex, subcategoryIndex)">
+                            {{subcategory.name}}
+                          </li>
+                        </ul>
                       </li>
                     </ul>
                   </div>
-                  <ul class ="noBullets">
-                    <li v-for="(category, index) in categories">
-                      <input type="checkbox" v-on:click="getSubCategories(index)"  v-bind:checked="appliedCategories[category]">{{category.name}}
-                    </li>
-                  </ul>
                 </div>
             </div>
             <div class = "col-sm-9" id = "itemsContainer">
-
                 <div id="item-panel">
-                  <p id="resultsFoundText">{{items.length}} results found</p>
+                  <p id="resultsFoundText">{{displayedItems.length}} results found</p>
                   <ul class ="noBullets">
-                    <li v-for="item in items">
+                    <li v-for="item in displayedItems">
                       <Product
                         v-bind:id="item.id"
                         v-bind:price="item.price"
@@ -67,80 +66,66 @@ import Product from '@/components/Product.vue';
   }
 })
 export default class Shop extends App {
-  categories: CategoryItem[] = [];
-  appliedCategories: CategoryItem[] = [];
-  items: ShopItem[] = [];
-/*
-  createNewCategory(){
-    axios.put(`/api/categories`, {
-      username: this.UsernameMessage,
-      password: this.PasswordMessage,
-      lastName: this.LastNameMessage,
-      firstName: this.FirstNameMessage
-    }).then((res) => {
-    }
-  }
-  */
+  topLevelCategories: CategoryItem[] = [];
+  idsOfAppliedCategories: [number, number][] = [];
+  displayedItems: ShopItem[] = [];
+  loadedTopLevelCategoryCount: number = 0;
 
-  getTopLevelCategories(){
+  getAllTopLevelCategories(){
     axios.get(`/api/categories/parents`)
     .then((res) => {
-      this.categories = res.data.categories;
+      this.topLevelCategories = res.data.categories;
+      for(var i = 0; i < this.topLevelCategories.length; i++){
+        this.getSubLevelCategories(i);
+      }
     })
   }
 
-  getSubCategories(index){
-    this.appliedCategories.push(this.categories[index]);
-    this.getSpecificCategoryItems(index);
-    axios.get(`/api/categories/parent/` + this.categories[index].id)
+  getSubLevelCategories(index){
+    this.topLevelCategories[index].subcategories = [];
+    axios.get(`/api/categories/parent/` + this.topLevelCategories[index].id)
     .then((res) => {
-      this.categories = res.data.category;
+      this.topLevelCategories[index].subcategories = res.data.category;
+      this.loadedTopLevelCategoryCount++; // only render when all responses have got back
     })
-  }
-
-  getSuperCategories(index){
-    /*
-     * cut off a part of the applied categories
-     */
-    var temp = this.appliedCategories;
-    this.topCategory = this.categories[index];
-    this.appliedCategories = [];
-    for(var i = 0; i < index; i++){
-      this.appliedCategories.push(temp[i]);
-    }
-    // get the subcategories of the new last category
-    if(index > 0){
-      this.categories = this.appliedCategories;
-      this.getSpecificCategoryItems(index-1);
-      axios.get(`/api/categories/parent/` + this.appliedCategories[index-1].id)
-      .then((res) => {
-        this.categories = res.data.category;
-      })
-    }
-    // or all categories if all categories have removed
-    else{
-      this.getTopLevelCategories();
-      this.getAllItems();
-    }
-  }
-
-  beforeMount(){
-    this.getTopLevelCategories()
-    this.getAllItems()
   }
 
   getAllItems(){
-    axios.get(`/api/items`)
-    .then((res) => {
-      this.items = res.data.items;
-    })
+   axios.get(`/api/items`)
+   .then((res) => {
+     this.displayedItems = res.data.items;
+   })
+ }
+
+ getItemsRefinedByCategories(categoryIndex, subcategoryIndex){
+   this.categoryChanged(categoryIndex, subcategoryIndex);
+   this.displayedItems = [];
+   if(this.idsOfAppliedCategories.length == 0){
+     this.getAllItems();
+     return;
+   }
+   for(var i = 0; i < this.idsOfAppliedCategories.length; i++){
+     var idTuple = this.idsOfAppliedCategories[i];
+     axios.get(`/api/items/byCat/` + this.topLevelCategories[idTuple[0]].subcategories[idTuple[1]])
+     .then((res) => {
+        this.displayedItems.push(res.data.items);
+     })
+   }
+ }
+
+  categoryChanged(index1, index2){
+    for(var i = 0; i < this.idsOfAppliedCategories.length; i++){
+      if(this.idsOfAppliedCategories[i][0] == index1 && this.idsOfAppliedCategories[i][1] == index2){
+        this.idsOfAppliedCategories.splice(i, 1);
+        return;
+      }
+    }
+    this.idsOfAppliedCategories.push([index1, index2]);
   }
 
-  getSpecificCategoryItems(index){
-    axios.get(`/api/items/byCat/` + this.categories[index].id)
-    .then((res) => {
-      this.items = res.data.items;
-    })
+  beforeMount(){
+    this.getAllTopLevelCategories();
+    this.getAllItems();
   }
 }
 </script>
@@ -159,8 +144,6 @@ export default class Shop extends App {
 
 #category-bar{
   height: 100%;
-
-
 
   ul {
     list-style: none;
