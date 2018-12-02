dab611b47448019e59e1b6d9787e3bc5b1b149dd
Kyle Chin <kylechin01@gmail.com>
2018-12-02 03:11:16 -0800

    I am typing words with my hands: more admin order management stuff - it should be done now

--- app/src/views/AdminOrderManagement.vue
+++ app/src/views/AdminOrderManagement.vue
@@ -1,22 +1,47 @@
 <template>
   <div id="admin-order-management">
     Shipping
-    <li v-for="(order, orderIndex) in shippingOrders" v-if="numberOfLoadedItemSets == shippingOrders.length">
+    <li v-for="(order, orderIndex) in shippingOrders"
+    v-if="itemsAssociatedWithOrders[0].length == shippingOrders.length && usersAssociatedWithOrders[0].length == shippingOrders.length &&
+         orderItemsAssociatedWithOrders[0].length == shippingOrders.length">
       {{order}}
+      {{usersAssociatedWithOrders[0][orderIndex]}}
       <button type="submit" v-on:click="cancelOrder(orderIndex)">Cancel Order</button>
       <ul>
-        <li v-for="(item, itemIndex) in itemsAssociatedWithShippingOrders[orderIndex]">
+        <li v-for="(item, itemIndex) in itemsAssociatedWithOrders[0][orderIndex]">
           {{item}}
+          {{orderItemsAssociatedWithOrders[0][orderIndex][itemIndex]}}
         </li>
       </ul>
     </li>
     Completed
-    <li v-for="(order, index) in completedOrders">
+    <li v-for="(order, orderIndex) in completedOrders"
+    v-if="itemsAssociatedWithOrders[1].length == completedOrders.length && usersAssociatedWithOrders[1].length == completedOrders.length &&
+         orderItemsAssociatedWithOrders[1].length == completedOrders.length">
       {{order}}
+      {{usersAssociatedWithOrders[1][orderIndex]}}
+      <button type="submit" v-on:click="cancelOrder(orderIndex)">Cancel Order</button>
+      <ul>
+        <li v-for="(item, itemIndex) in itemsAssociatedWithOrders[1][orderIndex]">
+          {{item}}
+          {{orderItemsAssociatedWithOrders[1][orderIndex][itemIndex]}}
+        </li>
+      </ul>
     </li>
     Cancelled
-    <li v-for="(order, index) in cancelledOrders">
+    {{orderItemsAssociatedWithOrders[2]}}
+    <li v-for="(order, orderIndex) in cancelledOrders"
+    v-if="itemsAssociatedWithOrders[2].length == cancelledOrders.length && usersAssociatedWithOrders[2].length == cancelledOrders.length &&
+         orderItemsAssociatedWithOrders[2].length == cancelledOrders.length">
       {{order}}
+      {{usersAssociatedWithOrders[2][orderIndex]}}
+      <button type="submit" v-on:click="cancelOrder(orderIndex)">Cancel Order</button>
+      <ul>
+        <li v-for="(item, itemIndex) in itemsAssociatedWithOrders[2][orderIndex]">
+          {{item}}
+          {{orderItemsAssociatedWithOrders[2][orderIndex][itemIndex]}}
+        </li>
+      </ul>
     </li>
   </div>
 </template>
@@ -24,24 +49,39 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import axios from 'axios';
-import { OrderItem, ShopItem } from '@/models';
+import { OrderItem, ShopItem, CreateUserItem, OrderOrderItems } from '@/models';
 
 @Component
 export default class AdminOrderManagement extends Vue {
   allOrders: OrderItem[] = [];
   shippingOrders: OrderItem[] = [];
-  itemsAssociatedWithShippingOrders: [ShopItem[]] = [[]];
   completedOrders: OrderItem[] = [];
   cancelledOrders: OrderItem[] = [];
-  numberOfLoadedItemSets: number = 0;
+  orderItemsAssociatedWithOrders: [OrderOrderItems[]] = [[]];
+  itemsAssociatedWithOrders: [ShopItem[]] = [[]];
+  usersAssociatedWithOrders: [CreateUserItem[]] = [[]];
 
   cancelOrder(indexIntoShippingOrders){
     axios.patch(`/api/orders/` + this.shippingOrders[indexIntoShippingOrders].id + `/Cancelled`);
     // client side front end change
     this.$set(this.shippingOrders[indexIntoShippingOrders], "shippingStatus", "Cancelled");
     var transferredOrder = this.shippingOrders.splice(indexIntoShippingOrders, 1);
-    this.numberOfLoadedItemSets--;
     this.cancelledOrders.push(transferredOrder[0]);
+    this.itemsAssociatedWithOrders[0].splice(indexIntoShippingOrders, 1);
+    this.usersAssociatedWithOrders[0].splice(indexIntoShippingOrders, 1);
+    this.orderItemsAssociatedWithOrders[0].splice(indexIntoShippingOrders, 1);
+    // transfer all?
+  }
+
+  completedOrder(indexIntoShippingOrders){
+    axios.patch(`/api/orders/` + this.shippingOrders[indexIntoShippingOrders].id + `/Complete`);
+    // client side front end change
+    this.$set(this.shippingOrders[indexIntoShippingOrders], "shippingStatus", "Complete");
+    var transferredOrder = this.shippingOrders.splice(indexIntoShippingOrders, 1);
+    this.completedOrders.push(transferredOrder[0]);
+    this.itemsAssociatedWithOrders[0].splice(indexIntoShippingOrders, 1);
+    this.usersAssociatedWithOrders[0].splice(indexIntoShippingOrders, 1);
+    this.orderItemsAssociatedWithOrders[0].splice(indexIntoShippingOrders, 1);
   }
 
   getAllOrders(){
@@ -56,7 +96,6 @@ export default class AdminOrderManagement extends Vue {
     this.shippingOrders = [];
     this.completedOrders = [];
     this.cancelledOrders = [];
-    this.numberOfLoadedItemSets = 0;
     for(var i = 0; i < this.allOrders.length; i++){
       if(this.allOrders[i].shippingStatus == "Shipping"){
         this.shippingOrders.push(this.allOrders[i]);
@@ -68,18 +107,83 @@ export default class AdminOrderManagement extends Vue {
         this.cancelledOrders.push(this.allOrders[i]);
       }
     }
-    this.itemsAssociatedWithOrders = new Array(this.shippingOrders.length);
+    this.orderItemsAssociatedWithOrders = [[], [], []];
+    this.itemsAssociatedWithOrders = [[], [], []];
+    this.usersAssociatedWithOrders = [[], [], []];
     for(var i = 0; i < this.shippingOrders.length; i++){
-      this.getItemsAssociatedWithShippingOrder(i);
+      this.getItemsAssociatedWithOrder(i, "Shipping");
+      this.getUsersAssociatedWithOrder(i, "Shipping");
+      this.getOrderItemsAssociatedWithOrder(i, "Shipping");
+    }
+    for(var i = 0; i < this.completedOrders.length; i++){
+      this.getItemsAssociatedWithOrder(i, "Completed");
+      this.getUsersAssociatedWithOrder(i, "Completed");
+      this.getOrderItemsAssociatedWithOrder(i, "Completed");
+    }
+    for(var i = 0; i < this.cancelledOrders.length; i++){
+      this.getItemsAssociatedWithOrder(i, "Cancelled");
+      this.getUsersAssociatedWithOrder(i, "Cancelled");
+      this.getOrderItemsAssociatedWithOrder(i, "Cancelled");
     }
   }
 
-  getItemsAssociatedWithShippingOrder(orderIndex){
-    console.log(this.shippingOrders[orderIndex].id);
-    axios.get(`/api/orders/items/` + this.shippingOrders[orderIndex].id)
+  getItemsAssociatedWithOrder(orderIndex, orderStatus){
+    var orderList, itemArrayIndex;
+    if(orderStatus == "Shipping"){
+      orderList = this.shippingOrders;
+      itemArrayIndex = 0;
+    }
+    else if(orderStatus == "Completed"){
+      orderList = this.completedOrders;
+      itemArrayIndex = 1;
+    }
+    else if(orderStatus == "Cancelled"){
+      orderList = this.cancelledOrders;
+      itemArrayIndex = 2;
+    }
+    axios.get(`/api/orders/items/` + orderList[orderIndex].id)
     .then((res) => {
-      this.itemsAssociatedWithShippingOrders[orderIndex] = res.data;
-      this.numberOfLoadedItemSets++;
+      this.itemsAssociatedWithOrders[itemArrayIndex].push(res.data);
+    });
+  }
+
+  getUsersAssociatedWithOrder(orderIndex, orderStatus){
+    var orderList, userArrayIndex;
+    if(orderStatus == "Shipping"){
+      orderList = this.shippingOrders;
+      userArrayIndex = 0;
+    }
+    else if(orderStatus == "Completed"){
+      orderList = this.completedOrders;
+      userArrayIndex = 1;
+    }
+    else if(orderStatus == "Cancelled"){
+      orderList = this.cancelledOrders;
+      userArrayIndex = 2;
+    }
+    axios.get(`/api/users/` + orderList[orderIndex].UserId)
+    .then((res) => {
+      this.usersAssociatedWithOrders[userArrayIndex].push(res.data.user);
+    });
+  }
+
+  getOrderItemsAssociatedWithOrder(orderIndex, orderStatus){
+    var orderList, orderItemArrayIndex;
+    if(orderStatus == "Shipping"){
+      orderList = this.shippingOrders;
+      orderItemArrayIndex = 0;
+    }
+    else if(orderStatus == "Completed"){
+      orderList = this.completedOrders;
+      orderItemArrayIndex = 1;
+    }
+    else if(orderStatus == "Cancelled"){
+      orderList = this.cancelledOrders;
+      orderItemArrayIndex = 2;
+    }
+    axios.get(`/api/orderItems/` + orderList[orderIndex].id)
+    .then((res) => {
+      this.orderItemsAssociatedWithOrders[orderItemArrayIndex].push(res.data.items);
     });
   }
 
