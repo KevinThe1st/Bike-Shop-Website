48fddb315ff26fa8e2d7cf1e895369e38886792d
Kyle Chin <kylechin01@gmail.com>
2018-12-01 23:51:55 -0800

    fixing the admin order management page

--- app/src/views/AdminOrderManagement.vue
+++ app/src/views/AdminOrderManagement.vue
@@ -27,7 +27,7 @@ import axios from 'axios';
 import { OrderItem, ShopItem } from '@/models';
 
 @Component
-export default class Home extends Vue {
+export default class AdminOrderManagement extends Vue {
   allOrders: OrderItem[] = [];
   shippingOrders: OrderItem[] = [];
   itemsAssociatedWithShippingOrders: [ShopItem[]] = [[]];
@@ -40,6 +40,7 @@ export default class Home extends Vue {
     // client side front end change
     this.$set(this.shippingOrders[indexIntoShippingOrders], "shippingStatus", "Cancelled");
     var transferredOrder = this.shippingOrders.splice(indexIntoShippingOrders, 1);
+    this.numberOfLoadedItemSets--;
     this.cancelledOrders.push(transferredOrder[0]);
   }
 
