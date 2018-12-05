c2abed0c0219788712a1fb47fddbd2c5e4f75278
jccho16 <jccho@calpoly.edu>
2018-12-04 17:23:04 -0800

    Finished textbox tests + updated routes

--- server/test/truncate.js
+++ server/test/truncate.js
@@ -1,6 +1,6 @@
-const { User, Address, Item, Order, OrderItem, Session, Category, ItemCategory } = require('../models');
+const { User, Address, Item, Order, OrderItem, Session, Category, ItemCategory, Textbox} = require('../models');
 const { Op } = require('sequelize');
-const models = [User, Address, Item, Order, OrderItem, Session, Category, ItemCategory];
+const models = [User, Address, Item, Order, OrderItem, Session, Category, ItemCategory, Textbox];
 
 module.exports = function truncate() {
   return Promise.all(
